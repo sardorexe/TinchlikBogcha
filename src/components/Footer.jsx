5ed67bfbffd763bf147fa04abfc684911638ce9b
sardorexe <vapaevsad@gmail.com>
2021-06-15 22:46:56 +0500

    Cahnges

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,25 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <footer style={{display: "flex", alignItems: "center", flexDirection: "column", marginBottom: "10px"}} className="footer">
+    <footer
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        marginBottom: "10px",
+      }}
+      className="footer"
+    >
       <span>
-        &copy; Xorazm viloyati, Gurlan tumani 5-son "Tinchlik" maktabgacha ta'lim
-        muassasasi
+        &copy; Xorazm viloyati, Gurlan tumani 5-son "Tinchlik" maktabgacha
+        ta'lim tashkiloti
       </span>
-      <span>Dasturchi: <a target="_blank" href="https://instagram.com/sardor_insane">Sardor</a></span>
+      <span>
+        Dasturchi:{" "}
+        <a target="_blank" href="https://instagram.com/sardor_insane">
+          Sardor
+        </a>
+      </span>
     </footer>
   );
 }
